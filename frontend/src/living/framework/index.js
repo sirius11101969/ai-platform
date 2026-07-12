d407e48f58d88aafd41860b444a8800605a62ed3
Vladimir Shishkov <35781710+sirius11101969@users.noreply.github.com>
2026-07-12 12:54:24 +0700

    feat: export Living Geometry renderer

--- frontend/src/living/framework/index.js
+++ frontend/src/living/framework/index.js
@@ -1,4 +1,5 @@
 export { LivingFrame } from "./LivingFrame.jsx";
 export { LivingInput } from "./LivingInput.jsx";
+export { LivingGeometry } from "./LivingGeometry.jsx";
 export { livingSpaceRegistry, getLivingSpaceDefinition } from "./spaceRegistry.js";
 export { livingSpaceTokens } from "./tokens.js";
