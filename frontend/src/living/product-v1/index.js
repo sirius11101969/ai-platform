a9545645ea8a81efceb0e208b55ed9ebf970d406
Vladimir Shishkov <35781710+sirius11101969@users.noreply.github.com>
2026-07-12 17:18:27 +0700

    feat: export AS6 Living Product v1

---
+++ frontend/src/living/product-v1/index.js
@@ -0,0 +1,3 @@
+export { LivingProduct } from "./LivingProduct.jsx";
+export { initialLivingProductContext, reduceLivingProductContext } from "./productContext.js";
+export { livingProductV1Spaces, assertLivingProductSpace } from "./productSpaces.js";
