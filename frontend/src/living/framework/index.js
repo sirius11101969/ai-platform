9bd1f2c4ae52eb30fd0b45e4b8048d3fa04831b1
Vladimir Shishkov <35781710+sirius11101969@users.noreply.github.com>
2026-07-12 12:15:40 +0700

    feat: export Living Space framework

---
+++ frontend/src/living/framework/index.js
@@ -0,0 +1,4 @@
+export { LivingFrame } from "./LivingFrame.jsx";
+export { LivingInput } from "./LivingInput.jsx";
+export { livingSpaceRegistry, getLivingSpaceDefinition } from "./spaceRegistry.js";
+export { livingSpaceTokens } from "./tokens.js";
